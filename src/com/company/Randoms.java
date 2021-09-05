fac08f99a2b82b3faf2cfd28478df339f7b61672
RustemGaniev <ganievrr@gmail.com>
2021-09-05 21:19:00 +0300

    My edition

--- src/com/company/Randoms.java
+++ src/com/company/Randoms.java
@@ -1,33 +1,23 @@
 package com.company;
 
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Random;
-import java.util.function.Consumer;
 
 public class Randoms implements Iterable<Integer> {
 
     int max;
     int min;
 
-public Randoms(int min, int max){
-    this.min = min;
-    this.max = max;
+    public Randoms(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
 
-}
-
-public Randoms() {
-
-}
-
-
-    public int Randomms(Randoms random) {
+    public int randoms(Randoms random) {
         Random a = new Random();
         int dif = random.max - random.min;
         int i = a.nextInt(dif + 1);
         return random.min + i;
-
     }
 
     @Override
@@ -38,26 +28,14 @@ public Randoms() {
 
             @Override
             public boolean hasNext() {
-                    return true;
-                }
-
+                return true;
+            }
 
             @Override
             public Integer next() {
-                Integer i = Randomms(new Randoms(90, 100));
+                Integer i = randoms(new Randoms(90, 100));
                 return i;
-            }
-
-            @Override
-            public void remove() {
-                Iterator.super.remove();
-            }
-
-            @Override
-            public void forEachRemaining(Consumer<? super Integer> action) {
-                Iterator.super.forEachRemaining(action);
             }
         };
     }
-
 }
