114e09d5c80ac2f23367a911b5649548a5123867
RustemGaniev <ganievrr@gmail.com>
2021-09-07 20:09:54 +0300

    Philip Voronov Next Modigication Edition

--- src/com/company/Randoms.java
+++ src/com/company/Randoms.java
@@ -7,17 +7,11 @@ public class Randoms implements Iterable<Integer> {
 
     int max;
     int min;
+    int i;
 
     public Randoms(int min, int max) {
         this.min = min;
         this.max = max;
-    }
-
-    public int randoms(Randoms random) {
-        Random a = new Random();
-        int dif = random.max - random.min;
-        int i = a.nextInt(dif + 1);
-        return random.min + i;
     }
 
     @Override
@@ -33,7 +27,6 @@ public class Randoms implements Iterable<Integer> {
 
             @Override
             public Integer next() {
-                Integer i = randoms(new Randoms(90, 100));
                 return i;
             }
         };
